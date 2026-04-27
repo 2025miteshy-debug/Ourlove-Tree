d9f5e9f155e6e00212503c8e501984840673027b
2025miteshy-debug <2025.miteshy@isu.ac.in>
2026-04-27 21:58:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,14 @@
 var config = {
   names: ["Mitesh ❤️", "Meri Jaan 💖"],
 
-  date: "2025-01-14 00:00:00",
+  date: "2025/01/14 00:00:00",
 
   lines: [
     "Hey Jaan ❤️",
-    "Yrr pata hai bahut yaad aati hai aapki 😔",
-    "Aur main aapko bahut miss karta hu 💔",
-    "Bas hamesha yehi sochta rehta hu...",
+    "Bahut yaad aati hai aapki 😔",
+    "Main aapko bahut miss karta hu 💔",
     "Kab aap mere paas aaoge ❤️",
-    "Hamesha ke liye aur fir kabhi hume milne ka tension nahi hoga 💫",
-    "Sacchi jaan bahut yaad aati hai aapki 🥺",
-    "Aapko bahut jaldi se milna hai 💖",
-    "Main aapse milne ka wait nahi kar pa raha 😭",
-    "I really miss youuuuu Baachaaw 🥺❤️",
-    "I really love you ❤️"
+    "Hamesha ke liye 💫",
+    "I love you forever ❤️"
   ]
 }
