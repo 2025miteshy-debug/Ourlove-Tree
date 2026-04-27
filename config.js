b7c94db658997173f1dfbb9f1c4ee71e813084c6
2025miteshy-debug <2025.miteshy@isu.ac.in>
2026-04-27 22:02:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,13 @@ var config = {
   date: "2025/01/14 00:00:00",
 
   lines: [
-    "Hey Jaan ❤️",
+    "Hey Baachaw ❤️",
     "Bahut yaad aati hai aapki 😔",
     "Main aapko bahut miss karta hu 💔",
     "Kab aap mere paas aaoge ❤️",
     "Hamesha ke liye 💫",
+    "Aur fir kabhi bhi milne ka tension nhi hoga hame saachi yrr 🥺"
+    "I miss youuuuu baachaw soo muchh 🥺"
     "I love you forever ❤️"
   ]
 }
