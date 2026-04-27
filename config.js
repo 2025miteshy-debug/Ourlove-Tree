4428de86de6700aa4eaccb42a3ff2e2e374ef2ec
2025miteshy-debug <2025.miteshy@isu.ac.in>
2026-04-27 21:03:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 var config = {
-  name: "Teri Name ❤️",
+  name: "Miteshh ❤️",
   loveName: "Meri Jaan",
-  startDate: "2023-01-01",
+  startDate: "2025-01-14",
   message: `
 Hey Jaan ❤️
 
-Jab se tu meri life me aayi hai,
-sab kuch better ho gaya hai.
-
-Tu meri aadat ban chuki hai…
-aur ab bina tere din complete nahi lagta 💫
-
-Distance hai par tu hamesha paas feel hoti hai.
-
+Yrr pta hai bahut yaad aati hai aapki aur bahut miss krta hu aapko
+bas hamesha yhi sochta reheta hu ke kab aap mere pass aajaoge 
+hamesha ke liye aur firr kabhi hame milne ka tension nhi hoga 
+saachi jaan bahut yaad aati hai aapki bahut jayada jldi se 
+aajan yrr merse milne bahut mn hai aapse milne ka muje aur 
+bahut yaad aa rhi hai aapki I reall miss youuuuuu Baachaw 🥺😩
 I really love you ❤️
 `
 }
